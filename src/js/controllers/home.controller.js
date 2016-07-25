87143fd96ed2192f9ec74d39d9e24005eb94159a
Christopher Herrington <christopher.herrington@outlook.com>
2016-07-25 03:14:22 -0400

    server sync code added

--- src/js/controllers/home.controller.js
+++ src/js/controllers/home.controller.js
@@ -9,9 +9,34 @@ function HomeController ($scope, $http, SERVER, $state) {
       $scope.photos = res.data;
       console.log("HOME: ", $scope.photos)
     });
+    // window.setInterval(sync, 5000);
   }
+  //
+  // function sync() {
+  //   console.log($scope.photos)
+  //   $http.post(SERVER.URL, $scope.photos)
+  // }
 
-}
+  $scope.displayLike = function(photoObj, likes) {
+  switch (likes) {
+    case undefined:
+      console.log(photoObj)
+      // $http.post(SERVER.URL, photoObj)
+    return 0 + " Likes"
+
+    case 1:
+      return likes + " Like"
+      break;
+    default:
+      return likes + " Likes"
+      }
+
+
+
+  // $http.post(SERVER.URL, photo)
+    }
+
+  }
 
 HomeController.$inject = ['$scope', '$http', 'SERVER', '$state'];
 export { HomeController };
--- src/js/controllers/upload.controller.js
+++ src/js/controllers/upload.controller.js
@@ -1,9 +1,11 @@
 function UploadController ($scope, $http, SERVER, $state) {
-
+$scope.photo = {}
+$scope.photo.likes = 0
 
   $scope.addPhoto = (photo) => {
     $http.post(SERVER.URL, photo).then( (res) => {
       $state.go('root.home');
+      console.log("photo", photo)
     });
   };
 }
